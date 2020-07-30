1a791a1111dcb34c198f98e267051fa3a50ebd3a
AdnanGondal <adnangondal1@hotmail.com>
2020-07-30 16:00:28 +0100

    Adjustable size

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,24 @@
 
-createGrid(16);
+createGrid(50);
 
 
 function createGrid(size) {
     const container = document.querySelector('#container');
-    
+
 
     for (let i=1;i<=size;i++) {
         const row = document.createElement('div');
         row.classList.toggle('row');
         row.classList.toggle('square');
+        //row.style.height = `${600/size}px`
+        row.style.width = `${400/size}px`
         //div.textContent = i;
         for (let j=1;j<=size;j++){
             const col = document.createElement('div');      
             col.classList.toggle('column');
             col.classList.toggle('square');
+            col.style.height = `${400/size}px`
+            //col.style.width = `${600/size}px`
             row.appendChild(col);
         }
         container.appendChild(row);
